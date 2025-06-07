dae82e23b325db5ed9d02506f737e9289418a6be
Ricardo Louro <ricardo.f.louro@gmail.com>
2025-06-07 02:27:46 +0100

    Update comment to HealthPickup.cs. Update README.md

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class HealthPickup : Pickable
 {
+    //Set the amount of health this pickable should recover
     [SerializeField] private int amount;
+
     protected override void Pickup(Collider collision)
     {
+        //Try and extract the HealthSystem of the object it collided with
         HealthSystem healthSystem = collision.GetComponent<HealthSystem>();
+        //If it does have a HealthSystem... (this means it is a Player)
         if (healthSystem != null)
         {
+            //Recover the Player's health by the specified amount.
             healthSystem.GainHealth(amount);
         }
     }
--- README.md
+++ README.md
@@ -20,6 +20,12 @@ Esta pode ser rodada através de movimento do rato e atualiza a rotação do jogado
 ### GroundCheck.cs
 Este script informa o jogador controlado pelo cliente local (e apenas esse jogador) se está em contact com o chão ou não através do valor de um bool contido no PlayerMovement.
 
+### HealthPickup.cs - UNUSED
+Este script herda da classe Pickable e seria utilizado na criação de um item que recupera vida ao jogador.
+O script é funcional e existe esse item nos Prefabs no jogo mas devido ao game design, não foi utilizado.
+
+
+
 ### NetworkSetup.cs
 
 
